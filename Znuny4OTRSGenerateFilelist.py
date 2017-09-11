52b318121ed657586ea5b18d9d56f6f295d033cd
Thorsten Eckel <te@znuny.com>
2017-09-11 15:23:23 +0200

    Fixed bug: New file permissions are only applied for OTRS >= 6.

--- Znuny4OTRSGenerateFilelist.py
+++ Znuny4OTRSGenerateFilelist.py
@@ -21,6 +21,21 @@ class Znuny4OtrsGenerateFilelist(sublime_plugin.TextCommand):
 
   def generate_filelist(self, folder):
 
+    view_content = self.view.substr(sublime.Region(0, self.view.size()))
+
+    m = re.search(r'<Framework>([^<]+)', view_content)
+    if not m:
+      return
+
+    otrs_version = m.group(1)
+
+    exec_permission    = '755'
+    regular_permission = '644'
+
+    if otrs_version == '6.0.x':
+      exec_permission    = '770'
+      regular_permission = '660'
+
     files = []
     for (dirpath, dirnames, filenames) in walk(folder):
 
@@ -55,12 +70,12 @@ class Znuny4OtrsGenerateFilelist(sublime_plugin.TextCommand):
     for file_location in sorted(files, key=str.lower):
       file_entry = file_entry_template
 
-      permission = '660'
+      permission = regular_permission
       if re.search(r'^bin/', file_location):
-        permission = '770'
+        permission = exec_permission
 
       if re.search(r'\.sh$', file_location):
-        permission = '770'
+        permission = exec_permission
 
       if os.path.sep != '/':
         file_location = file_location.replace(os.path.sep, '/')
