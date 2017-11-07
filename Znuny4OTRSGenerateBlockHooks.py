8710ba64828a311afe9f742b94952789df5cb721
Thorsten Eckel <te@znuny.com>
2017-11-07 16:10:04 +0100

    Added support for new OTRS 6 config format.

--- Znuny4OTRSGenerateBlockHooks.py
+++ Znuny4OTRSGenerateBlockHooks.py
@@ -1,7 +1,53 @@
 import sublime, sublime_plugin, re
 
 class Znuny4OtrsGenerateBlockHooksCommand(sublime_plugin.TextCommand):
+
+  templates = {
+    'OTRS 6 or above': {
+      'pre': """<Setting Name="Frontend::Template::GenerateBlockHooks###100-Znuny4OTRSGenerateBlockHooks" Required="0" Valid="1">
+    <Description Translatable="1">Generate HTML comment hooks for the specified blocks so that filters can use them.</Description>
+    <Navigation>Core::Znuny4OTRS::Customizations::GenerateBlockHooks</Navigation>
+    <Value>
+        <Hash>
+            <Item Key=\"{template_name}\">
+                <Array>
+""",
+      'post': """</Array>
+            </Item>
+        </Hash>
+    </Value>
+</Setting>"""
+    },
+    'OTRS 6 or below': {
+      'pre': """<ConfigItem Name="Frontend::Template::GenerateBlockHooks###100-Znuny4OTRSGenerateBlockHooks" Required="1" Valid="1">
+    <Description Translatable="1">Generate HTML comment hooks for the specified blocks so that filters can use them.</Description>
+    <Group>Znuny4OTRSGenerateBlockHooks</Group>
+    <SubGroup>Core</SubGroup>
+    <Setting>
+        <Hash>
+            <Item Key=\"{template_name}\">
+                <Array>
+""",
+      'post': """</Array>
+            </Item>
+        </Hash>
+    </Setting>
+</ConfigItem>"""
+    },
+  }
+
   def run(self, edit):
+    self.edit = edit
+    sublime.active_window().show_quick_panel(list(self.templates.keys()), self.template_selected)
+
+  def template_selected(self, index):
+
+    otrs_template = list(self.templates.keys())[index]
+
+    self.view.run_command("znuny4_otrs_generate_block_hooks_sub", self.templates[otrs_template])
+
+class Znuny4OtrsGenerateBlockHooksSubCommand(sublime_plugin.TextCommand):
+  def run(self, edit, pre, post):
 
     # get file name/path of current opened view
     file_path = self.view.file_name()
@@ -17,17 +63,8 @@ class Znuny4OtrsGenerateBlockHooksCommand(sublime_plugin.TextCommand):
     template_name = template_name_match.group(1)
 
     # start XML template stub and add template name
-    config_template = """<ConfigItem Name="Frontend::Template::GenerateBlockHooks###100-Znuny4OTRSGenerateBlockHooks" Required="1" Valid="1">
-    <Description Translatable="1">Generate HTML comment hooks for the specified blocks so that filters can use them.</Description>
-    <Group>Znuny4OTRSGenerateBlockHooks</Group>
-    <SubGroup>Core</SubGroup>
-    <Setting>
-        <Hash>
-            <Item Key=\""""
-    config_template += template_name
-    config_template += """\">
-                <Array>
-"""
+    config_template = pre
+    config_template = config_template.replace('{template_name}', template_name)
 
     # loop over all "RenderBlockStart" blocks via a RegExp
     # and add an <Item></Item> - entry for each of them
@@ -39,11 +76,7 @@ class Znuny4OtrsGenerateBlockHooksCommand(sublime_plugin.TextCommand):
 
     # close XML template stub
     config_template += '                '
-    config_template += """</Array>
-            </Item>
-        </Hash>
-    </Setting>
-</ConfigItem>"""
+    config_template += post
 
     # open new temporary sublime view and add the XML template
     n = sublime.active_window().new_file()
