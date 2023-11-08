971ffda23e810ca50744e8b751349db4bb477886
Denny Korsukéwitz <dennykorsukewitz@gmail.com>
2023-11-08 08:53:36 +0000

    Version 1.0.10:
    Fixed - Codemarker is not accurate enough.
    Added LanguageID detection for QuoteWithMarker.

--- ZnunyQuoteWithMarker.py
+++ ZnunyQuoteWithMarker.py
@@ -1,74 +1,90 @@
+from datetime import date
+
 import sublime
 import sublime_plugin
-import re
 
 
 def plugin_loaded():
     global settings
-    settings = sublime.load_settings('Znuny.sublime-settings')
+    settings = sublime.load_settings("Znuny.sublime-settings")
 
 
 class ZnunyQuoteWithMarkerCommand(sublime_plugin.TextCommand):
     def run(self, edit):
 
-        # get quoting char
-        quote_char = None
-        syntax = self.view.settings().get('syntax')
-        znuny_code_marker = settings.get('znuny_code_marker') or "Znuny"
+        quote_char_start = ""
+        quote_char_end = ""
+        code_marker_replace = ""
 
-        if re.search("JavaScript", syntax):
-            quote_char = '//'
-        elif re.search("Perl", syntax):
-            quote_char = '#'
-        elif re.search("HTML", syntax):
-            quote_char = '#'
+        code_marker = settings.get("znuny_code_marker") or "Znuny"
+        current_time = date.today()
+        day = current_time.strftime("%d")
+        month = current_time.strftime("%m")
+        year = current_time.strftime("%Y")
+        code_marker = code_marker.replace("${year}", year)
+        code_marker = code_marker.replace("${month}", month)
+        code_marker = code_marker.replace("${day}", day)
 
-        if not quote_char or not znuny_code_marker:
-            return
+        meta = self.view.meta_info("shellVariables", 0)
+        for var in meta:
+            if var["name"] == "TM_COMMENT_START":
+                quote_char_start = var["value"]
 
-        # loop over all selections
+            if var["name"] == "TM_COMMENT_END":
+                quote_char_end = " " + var["value"]
+
+        # Loop over all selections.
         for region in self.view.sel():
 
-            # skip empty selections
+            # Skip empty selections.
             if region.empty():
                 next
 
-            # Get the selected text
+            # Get the selected text.
             selection = self.view.substr(region)
 
-            # start custom maker
-            code_marker_replace = """{quote_char} ---
-{quote_char} {znuny_code_marker}-
-{quote_char} ---
+            # Start custom maker.
+            code_marker_replace = """{quote_char_start} ---{quote_char_end}
+{quote_char_start} {code_marker}{quote_char_end}
+{quote_char_start} ---{quote_char_end}
 """
+            # Add QuoteCharStart to every single line.
+            for line in selection.split("\n"):
 
-            # loop over each selected line
-            for line in selection.split('\n'):
+                if len(line) == 0:
+                    continue
 
-                # add perl quote
-                code_marker_replace += '{quote_char}'
+                # Add quote.
+                code_marker_replace += "{quote_char_start}"
 
-                # insert leading space for non empty lines
+                # Insert leading space for non empty lines.
                 if len(line) >= 1:
-                    code_marker_replace += ' '
 
-                # add old line and an linebreak
-                code_marker_replace += line + "\n"
+                    code_marker_replace += " "
 
-            # add old selection and trailing code marker
+                # Add old line and an linebreak.
+                code_marker_replace += line + "{quote_char_end}\n"
+
+            # Add old selection and trailing code marker.
             code_marker_replace += selection
-            code_marker_replace += "\n\n{quote_char} ---"
+            code_marker_replace += "\n\n{quote_char_start} ---{quote_char_end}\n"
 
-            code_marker_replace = code_marker_replace.replace('{quote_char}', quote_char)
-            code_marker_replace = code_marker_replace.replace('{znuny_code_marker}', znuny_code_marker)
-    #        code_marker_replace.format(**replace_dict)
+            code_marker_replace = code_marker_replace.replace(
+                "{quote_char_start}", quote_char_start
+            )
+            code_marker_replace = code_marker_replace.replace(
+                "{quote_char_end}", quote_char_end
+            )
+            code_marker_replace = code_marker_replace.replace(
+                "{code_marker}", code_marker
+            )
 
-            # replace the selection with transformed text
+            # Replace the selection with transformed text
             self.view.replace(edit, region, code_marker_replace)
 
-        # clear selection regions / cursor position
+        # Clear selection regions / cursor position.
         self.view.sel().clear()
 
-        # set new regions to inserted custom marker package name
-        for begin in self.view.find_all("Znuny-\n"):
+        # Set new regions to inserted custom marker package name.
+        for begin in self.view.find_all(code_marker + "\n"):
             self.view.sel().add(sublime.Region(begin.a, begin.b - 1))
